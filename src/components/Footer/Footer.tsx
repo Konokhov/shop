d2f3f849767e8847303952879a80f8daa1b0a369
Sergey Konokhov <Konokhov@yandex.ru>
2023-09-24 19:15:46 +0300

    refactor: formatting footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import ContainerLayout from "components/ContainerLayout";
 import styles from "./Footer.module.scss";
 
 function Footer() {
-  return <footer className={styles.footer}> </footer>;
+  return (
+    <footer className={styles.footer}>
+      <ContainerLayout>2023</ContainerLayout>
+    </footer>
+  );
 }
 
 export default Footer;
