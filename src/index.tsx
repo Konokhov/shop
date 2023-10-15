9d88e0a9d28b2cf37b8a93375d3328c2aa235958
Sergey Konokhov <Konokhov@yandex.ru>
2023-10-15 14:31:01 +0300

    github pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/style.scss";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
